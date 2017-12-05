8730187c7e399aee29cf56579bd041cba7267d2f
Vincent van Dijk <info@vincentvandijk.com>
2017-12-05 19:24:51 +0100

    Added Task namespace.

---
+++ src/Task/TaskInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\Shell\Task;
+
+interface TaskInterface
+{
+    /**
+     * Execute task.
+     *
+     * @param array $data
+     * @return void
+     * @throws TaskException
+     */
+    public function execute(array $data): void;
+}

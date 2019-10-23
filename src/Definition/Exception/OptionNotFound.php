da8af53f95549cbc54427e73c91f7dc69a4c68be
Vincent van Dijk <info@vincentvandijk.com>
2019-10-23 18:24:05 +0200

    Added dashes as parameter.

--- src/Definition/Exception/OptionNotFound.php
+++ src/Definition/Exception/OptionNotFound.php
@@ -17,9 +17,10 @@ class OptionNotFound extends Exception implements DefinitionException
     public function __construct(string $name, bool $long = null)
     {
         parent::__construct(sprintf(
-            'No %s option found for name "%s".',
+            'No %s option found for name "%s%s".',
             $long ? 'long' : 'short',
-            ($long ? '--' : '-') . $name
+            $long ? '--' : '-',
+            $name
         ));
     }
 }
